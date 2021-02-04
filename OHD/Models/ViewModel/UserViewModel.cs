4a7582d6a2e25dcd4f32d5e60efd0508fb5cfd86
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2021-02-04 09:38:08 +0700

    fix

---
+++ OHD/Models/ViewModel/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OHD.Models.ViewModel
+{
+    public class UserViewModel
+    {
+    }
+}
