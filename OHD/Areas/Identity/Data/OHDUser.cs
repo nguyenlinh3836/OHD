3e03ef72e94c9cc4fb6e7205a9cab94c954498f9
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2021-02-26 16:09:25 +0700

    fix

--- OHD/Areas/Identity/Data/OHDUser.cs
+++ OHD/Areas/Identity/Data/OHDUser.cs
@@ -11,9 +11,27 @@ namespace OHD.Areas.Identity.Data
 {
     // Add profile data for application users by adding properties to the OHDUser class
     public class OHDUser : IdentityUser
-    {    
-        public Employee Employee { get; set; }  
-        public Customer Customer { get; set; }       
+    {
+        public int CustomerId { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string LastName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public string Gender { get; set; }
+        [Column(TypeName = "varchar(150)")]
+        public string Address { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string Contact { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string State { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string Country { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string City { get; set; }
+        public string RequestorID { get; set; }
+        public string AssigneeID { get; set; }
+        public ICollection<Request> Request { get; set; }
 
     }
 }
